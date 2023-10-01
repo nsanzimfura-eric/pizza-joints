4ef29e91a8e83e0cd00694f470ac4ef542790205
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-01 15:30:44 +0200

    add routing methods to change pages

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/home/home";
 import Lottie from "lottie-react";
 import loadingSpinner from "./assets/lotties/loadingSpinner.json";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [isPageLoading, setIsPageLoading] = useState(true);
@@ -13,16 +14,26 @@ function App() {
     }, 2000);
   }, []);
 
-  console.log(isPageLoading, "test");
-
   return isPageLoading ? (
     <Lottie animationData={loadingSpinner} />
   ) : (
-    <div className="w-full p-0 m-0 pt-10">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="w-full p-0 m-0 pt-10">
+        <Header />
+        <Routes>
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.chooseTYpe} element={<Home />} />
+          <Route path={routes.addIngredients} element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+const routes = {
+  home: "/",
+  chooseTYpe: "chose-type",
+  addIngredients: "add-Ingredients",
+};
