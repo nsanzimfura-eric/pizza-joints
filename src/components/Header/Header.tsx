c3b8fde51962e7e9c3bce49fb38f58d4717347c4
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-01 13:24:48 +0200

    create animation header and add whatsapp feature

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,36 @@
 import Lottie from "lottie-react";
 import logo from "../../assets/lotties/logo.json";
+import tsapp from "../../assets/lotties/tsapp.json";
 import "../styles.css";
 import { motion } from "framer-motion";
 
 function Header() {
   return (
-    <div className="w-full headerBox flex items-center justify-between p-5 m-0 pt-10">
-      <Lottie animationData={logo} className="logo" />
+    <div className="w-full headerBox flex items-center justify-between p-5 m-0 pt-10 shadow-md rounded-b-3xl">
+      <div className="flex flex-col gap-0  logo_container">
+        <Lottie animationData={logo} className="logo" />
+        <div className="text-white logoText flex">
+          PiZza <span className="singleLetter p-0 m-0">JO</span>InTs
+        </div>
+      </div>
       <div className="links flex gap-10 items-center">
-        <motion.a href="https://maps.app.goo.gl/2H7HscDamcbKtQ426">
+        <motion.a
+          href="https://maps.app.goo.gl/2H7HscDamcbKtQ426"
+          className="link text-sm md:text-base lg:text-lg hidden sm:block"
+        >
           Find us here
         </motion.a>
-        <motion.a href="https://maps.app.goo.gl/2H7HscDamcbKtQ426">
-          Call us
+        <motion.a
+          href="https://wa.me/+250735317999"
+          target="_blank"
+          className="link text-sm md:text-base lg:text-lg"
+        >
+          <Lottie
+            animationData={tsapp}
+            className="tsapp"
+            width={50}
+            height={50}
+          />
         </motion.a>
       </div>
     </div>
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,13 @@
 import "../styles.css";
 import { useRef, useState } from "react";
-import { motion } from "framer-motion";
-import { leftData, rightData } from "./dataDescription";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  leftData,
+  rightData,
+  menuVars,
+  containerVars,
+  titleAnim,
+} from "./dataDescription";
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import phoneAnim from "../../assets/lotties/phoneAnim.json";
 
@@ -27,41 +33,62 @@ function Home() {
           At your service, we deliver
         </motion.p>
       )}
-      <div className="showUpBoxes w-full flex items-center justify-center md:gap-20 lg:flex-row my-5">
-        <motion.div
-          className="flex flex-col animate-single-box justify-start items-start p-5 border-yellow-400 border bg-transparent rounded-lg md:rounded-xl lg:rounded-2xl xl:rounded-3xl text-white"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ duration: 2 }}
-        >
-          {leftData.map((item) => {
-            return (
-              <motion.p key={item} className="text-sm">
-                {item}
-              </motion.p>
-            );
-          })}
-        </motion.div>
-        <motion.div
-          className="display-none-one-426 animate-single-box p-5 flex flex-col justify-start items-start border-yellow-400 border bg-transparent rounded-lg md:rounded-xl lg:rounded-2xl xl:rounded-3xl text-white"
-          initial={{ x: "100vw" }}
-          animate={{ x: 0 }}
-          transition={{ duration: 2 }}
-        >
-          {rightData.map((item) => {
-            return (
-              <motion.p
-                initial={{ color: "#8a2ce2", opacity: 0.3 }}
-                animate={{ color: "white", opacity: 1 }}
-                key={item}
-                className="text-sm"
-              >
-                {item}
-              </motion.p>
-            );
-          })}
-        </motion.div>
-      </div>
+      <motion.div
+        variants={menuVars}
+        className="showUpBoxes w-full flex items-center justify-center md:gap-20 lg:flex-row my-5"
+      >
+        <AnimatePresence>
+          <motion.div
+            className="flex flex-col animate-single-box gap-3 justify-start items-start p-5 border-yellow-400 border bg-transparent rounded-lg md:rounded-xl lg:rounded-2xl xl:rounded-3xl text-white"
+            // initial={{ x: "-100vw" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 2 }}
+            variants={containerVars}
+            exit="initial"
+          >
+            {leftData.map((item) => {
+              return (
+                <div key={item}>
+                  <motion.p
+                    className="text-sm overflow-hidden uppercase"
+                    initial={{ color: "#8a2ce2", opacity: 0.3, y: "-50px" }}
+                    animate={{ color: "white", opacity: 1, y: 0 }}
+                    variants={titleAnim}
+                    transition={{ duration: 3 }}
+                  >
+                    {item}
+                  </motion.p>
+                </div>
+              );
+            })}
+          </motion.div>
+        </AnimatePresence>
+        <AnimatePresence>
+          <motion.div
+            className="display-none-one-426 gap-3 animate-single-box p-5 flex flex-col justify-start items-start border-yellow-400 border bg-transparent rounded-lg md:rounded-xl lg:rounded-2xl xl:rounded-3xl text-white"
+            // initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ duration: 2 }}
+            exit="initial"
+            variants={containerVars}
+          >
+            {rightData.map((item) => {
+              return (
+                <motion.p
+                  initial={{ color: "#8a2ce2", opacity: 0.3 }}
+                  animate={{ color: "white", opacity: 1 }}
+                  key={item}
+                  className="text-sm overflow-hidden uppercase"
+                  variants={titleAnim}
+                  transition={{ duration: 3 }}
+                >
+                  {item}
+                </motion.p>
+              );
+            })}
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
       <div className=" prepareBox w-full flex items-center justify-center p-0 m-0">
         <motion.div initial={{ y: "-50vh" }} animate={{ y: 0 }}>
           <Lottie
